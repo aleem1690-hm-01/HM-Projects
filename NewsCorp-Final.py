31d400623e538bc8582110ac97e1c597297ce719
aleem1690-hm-01 <152846166+aleem1690-hm-01@users.noreply.github.com>
2023-12-05 11:41:48 +0530

    Update NewsCorp-Final.py

--- NewsCorp-Final.py
+++ NewsCorp-Final.py
@@ -193,7 +193,7 @@ if __name__ == "__main__":
   mvp_player_name = st.text_input("Enter Player Name")
   # Radio button for selecting League
   league_options = ['AFL','BBL','NBL','NRL']
-  selected_league = st.radio("League", league_options)
+  selected_league = '''"'''+st.radio("League", league_options)+'''"'''
 
   # Input for entering Search Tags
   search_tags = st.text_input("Enter Search Tags")
